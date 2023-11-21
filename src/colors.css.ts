c643807c239327c20cc41544b498a890a93489d5
Aizer Ravelo <aizer@Aizers-MacBook-Pro.local>
2023-11-21 21:00:28 +0800

    First commit

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,4 +1,4 @@
-export const colors = {
+export const colorsOrig = {
   background: "#ffd500",
   text: "#005bbb",
   primary: "#005bbb",
@@ -6,3 +6,13 @@ export const colors = {
   active: "#004287",
   black: "#000",
 }
+
+export const colors = {
+  background: "#fff",
+  text: "#004ca3",
+  primary: "#004ca3",
+  muted: "#f5fcff",
+  active: "#001d3d",
+  black: "#000",
+}
+
